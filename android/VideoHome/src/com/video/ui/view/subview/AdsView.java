d1b5c5bf5d18fcfa1657af4f4def1970c8378d10
liuhuadong78@gmail.com <liuhuadong7804@gmail.com>
2014-11-17 18:20:20 +0800

    add flip for ads

--- android/VideoHome/src/com/video/ui/view/subview/AdsView.java
+++ android/VideoHome/src/com/video/ui/view/subview/AdsView.java
@@ -1,9 +1,14 @@
 package com.video.ui.view.subview;
 
 import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.*;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import com.squareup.picasso.Picasso;
 import com.tv.ui.metro.model.DisplayItem;
 import com.video.ui.R;
@@ -24,32 +29,77 @@ public class AdsView extends RelativeLayout{
         super(context, attrs, defStyle);
     }
 
-    private ViewFlipper viewFlipper;
     public AdsView(Context context, ArrayList<DisplayItem> items) {
         super(context, null, 0);
 
         initUI(items);
     }
 
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev){
+        boolean ret = super.dispatchTouchEvent(ev);
+        if(ret){
+            ((ViewGroup)viewFlipper.getParent()).requestDisallowInterceptTouchEvent(true);
+        }
+        return ret;
+    }
+
+    private  ViewPager viewFlipper;
+    private  ArrayList<DisplayItem> content;
+    private  ArrayList<View> viewList = new ArrayList<View>();
     private void initUI(ArrayList<DisplayItem> items){
+        content = items;
         View root = View.inflate(getContext(), R.layout.ads_viewflipper,  this);
-        viewFlipper = (ViewFlipper) root.findViewById(R.id.image_flipper);
+        viewFlipper = (ViewPager) root.findViewById(R.id.image_flipper);
+
         for(DisplayItem item: items) {
-            viewFlipper.addView(getImageView(item));
+            viewList.add(getImageView(item));
         }
 
-        viewFlipper.setOnClickListener(new OnClickListener() {
+        PagerAdapter pagerAdapter = new PagerAdapter() {
+
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getContext(), "touched", Toast.LENGTH_SHORT).show();
-                viewFlipper.showNext();
+            public boolean isViewFromObject(View arg0, Object arg1) {
+
+                return arg0 == arg1;
             }
-        });
+
+            @Override
+            public int getCount() {
+
+                return viewList.size();
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position,
+                                    Object object) {
+                container.removeView(viewList.get(position));
+
+            }
+
+            @Override
+            public int getItemPosition(Object object) {
+
+                return super.getItemPosition(object);
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return "";
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                container.addView(viewList.get(position));
+                return viewList.get(position);
+            }
+
+        };
+        viewFlipper.setAdapter(pagerAdapter);
     }
 
     private ImageView getImageView(DisplayItem item){
         ImageView imageView = new ImageView(getContext());
-        imageView.setFocusable(true);
         imageView.setBackgroundResource(R.drawable.list_selector_bg);
         Picasso.with(getContext()).load(item.images.get("poster").url).placeholder(R.drawable.icon_h_default).error(R.drawable.icon_h_default).fit().into(imageView);
         return imageView;
---
+++ android/VideoHome/src/com/video/ui/view/subview/CustomViewPager.java
@@ -0,0 +1,37 @@
+package com.video.ui.view.subview;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewParent;
+
+/**
+ * Created by liuhuadong on 11/17/14.
+ */
+public class CustomViewPager  extends ViewPager implements View.OnTouchListener {
+    public CustomViewPager(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+
+        setOnTouchListener(this);
+    }
+
+    public CustomViewPager(final Context context) {
+        super(context);
+        setOnTouchListener(this);
+    }
+
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                ((ViewParent) v.getParent()).requestDisallowInterceptTouchEvent(true);
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                ((ViewParent) v.getParent()).requestDisallowInterceptTouchEvent(false);
+                break;
+        }
+        return v.onTouchEvent(event);
+    }
+}
