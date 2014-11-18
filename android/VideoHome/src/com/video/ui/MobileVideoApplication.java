91946bac93f9a2ac171f6bd19714348876b6d848
liuhuadong78@gmail.com <liuhuadong7804@gmail.com>
2014-11-18 13:47:12 +0800

    fix for show default icon

---
+++ android/VideoHome/src/com/video/ui/MobileVideoApplication.java
@@ -0,0 +1,52 @@
+package com.video.ui;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.StatFs;
+import com.squareup.picasso.OkHttpDownloader;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+/**
+ * Created by liuhuadong on 11/18/14.
+ */
+public class MobileVideoApplication extends Application{
+    private static final int MIN_DISK_CACHE_SIZE = 5 * 1024 * 1024; // 5MB
+    private static final int MAX_DISK_CACHE_SIZE = 50 * 1024 * 1024; // 50MB
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        File cacheFile = createDefaultCacheDir(getApplicationContext());
+        Picasso picasso = new Picasso.Builder(getApplicationContext()).downloader(new OkHttpDownloader(cacheFile,calculateDiskCacheSize(cacheFile) )).build();
+        Picasso.setSingletonInstance(picasso);
+
+        Picasso.with(getApplicationContext()).setLoggingEnabled(true);
+        Picasso.with(getApplicationContext()).setIndicatorsEnabled(true);
+    }
+
+    static File createDefaultCacheDir(Context context) {
+        File cache = new File(context.getApplicationContext().getCacheDir(), "picasso-cache");
+        if (!cache.exists()) {
+            //noinspection ResultOfMethodCallIgnored
+            cache.mkdirs();
+        }
+        return cache;
+    }
+
+    static long calculateDiskCacheSize(File dir) {
+        long size = MIN_DISK_CACHE_SIZE;
+
+        try {
+            StatFs statFs = new StatFs(dir.getAbsolutePath());
+            long available = ((long) statFs.getBlockCount()) * statFs.getBlockSize();
+            // Target 2% of the total space.
+            size = available / 50;
+        } catch (IllegalArgumentException ignored) {
+        }
+
+        // Bound inside min/max size for disk cache.
+        return Math.max(Math.min(size, MAX_DISK_CACHE_SIZE), MIN_DISK_CACHE_SIZE);
+    }
+}
